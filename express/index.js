ded2e17fa7133fcdfedb468805fe8ab2c6fa4783
Abdul majeed <majeedzr7741@gmail.com>
2026-07-24 16:29:49 +0500

    domain added

--- express/index.js
+++ express/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:5173", "http://localhost:5174"];
+  : ["http://localhost:5173", "http://localhost:5174","https://bhatticlothing.store",];
 
   console.log("Allowed origins:", allowedOrigins);
 

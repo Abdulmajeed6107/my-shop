b641d78cb1d6d197d265cab7efd3a2019c874a7d
Abdul majeed <majeedzr7741@gmail.com>
2026-08-07 00:21:17 +0500

    whatsapp button added

--- newproject/src/App.jsx
+++ newproject/src/App.jsx
@@ -1,9 +1,13 @@
 import AppRoute from "./routes/AppRoutes";
+import WhatsAppButton from "./components/WhatsAppButton";
 
-export default function App() 
-{
-return(
-    
-<AppRoute />
-);
+export default function App() {
+    return (
+        <>
+            <AppRoute />
+            <WhatsAppButton />
+        </>
+
+
+    );
 }

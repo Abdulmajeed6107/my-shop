309617f41dffd9eb3ff342005d904547e2a3ec89
Abdul majeed <majeedzr7741@gmail.com>
2026-08-22 15:48:18 +0500

    cors error remove

--- express/index.js
+++ express/index.js
@@ -25,7 +25,8 @@ app.use(express.json());
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:5173", "http://localhost:5174", "https://bhatticlothing.store",];
+  : ["http://localhost:5173", "http://localhost:5174", "https://bhatticlothing.store",  'https://my-shop-ad.vercel.app'
+];
 
 console.log("Allowed origins:", allowedOrigins);
 
